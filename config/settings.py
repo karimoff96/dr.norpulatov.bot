19c9a75c1dce650131d9533dddbebef239c0d5b8
d_karimov <d.karimov@uzinfocom.uz>
2023-05-25 15:50:14 +0500

    allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['13.49.66.130', 'localhost', '127.0.0.1']
 
 
 # Application definition
