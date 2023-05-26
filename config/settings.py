1d740e8dfb47d7c358afbfeba55089fccf4ec57a
d_karimov <d.karimov@uzinfocom.uz>
2023-05-26 09:54:54 +0500

    domain

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.53.111.231', 'localhost', '127.0.0.1', 'norpolatov.uz', 'http://norpolatov.uz', 'https://norpolatov.uz']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'norpolatov.uz', 'wwww.norpolatov.uz']
 
 
 # Application definition
