18fbdbbb9dc0b7d7265d1cac5bd77135e743e902
d_karimov <d.karimov@uzinfocom.uz>
2023-07-10 20:09:01 +0500

    urls

--- config/urls.py
+++ config/urls.py
@@ -6,11 +6,11 @@ from django.conf.urls.static import static
 from bot import views, signals
 
 urlpatterns = [
-    path('index/', views.index, name='handler'),
-    path('cron/', signals.cron_job),
-    path('clear/', signals.clear_appointment),
-    path('rosetta/', include('rosetta.urls')),
-    path('send/', views.send_process),
+    path('bot/index/', views.index, name='handler'),
+    path('bot/cron/', signals.cron_job),
+    path('bot/clear/', signals.clear_appointment),
+    path('bot/rosetta/', include('rosetta.urls')),
+    path('bot/send/', views.send_process),
 ]
 if settings.DEBUG:
     urlpatterns += static( settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
