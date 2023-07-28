e11193d631e80eaa6e07e60d1d9fd127a6152266
d_karimov <d.karimov@uzinfocom.uz>
2023-07-28 14:24:22 +0500

    time change

--- bot/views.py
+++ bot/views.py
@@ -572,9 +572,10 @@ def show_available_times(user_id, chat_id, message_id):
         .distinct()
     )
 
-    current_time = (datetime.datetime.now() + datetime.timedelta(hours=5)).strftime(
-        "%H:%M"
-    )
+    # current_time = (datetime.datetime.now() + datetime.timedelta(hours=5)).strftime(
+    #     "%H:%M"
+    # )
+    current_time = datetime.datetime.now().strftime("%H:%M")
     print(current_time)
     booked_times = [time.strftime("%H:%M") for time in app]
     available_times = [time for time in available_times if time not in booked_times]
